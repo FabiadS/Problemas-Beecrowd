8cb316fb4507faf5573b649e2b4922edcdf573bb
Fabiola Spredemann <fabispredemann@gmail.com>
2022-07-26 12:29:34 -0300

    Problemas SQL

--- Problemas.sql
+++ Problemas.sql
@@ -106,3 +106,6 @@ where products.id_providers = providers.id and products.amount between '10' and 
 and providers.name like '%P%'
 
 --O setor de vendas quer fazer uma promoção para todos os clientes que são pessoas jurídicas. Para isso você deve exibir o nome dos clientes que sejam pessoa jurídica.
+select customers.name
+from customers, legal_person
+where customers.id = legal_person.id_customers

9b2471df5ffcb48b75e1e7c5539bd3981bedbde4
Fabiola Spredemann <fabispredemann@gmail.com>
2022-07-26 12:24:35 -0300

    Problemas SQL

--- Problemas.sql
+++ Problemas.sql
@@ -97,3 +97,12 @@ and p.price > 1000 and categories.name = 'Super Luxury'
 select cli.name, orders.id
 from customers as cli, orders
 where cli.id = orders.id_customers and orders.orders_date between '2016-01-01' and '2016-06-30'
+
+--Na hora de entregar o relatório de quantos produtos a empresa tem em estoque, uma parte do relatório ficou corrompida, por isso o responsável do estoque lhe pediu uma ajuda, ele quer que você exiba os seguintes dados para ele.
+--Exiba o nome dos produtos cujas quantidades estejam entre 10 e 20 e cujo nome do fornecedor inicie com a letra ‘P’.
+select products.name
+from products, providers
+where products.id_providers = providers.id and products.amount between '10' and '20'
+and providers.name like '%P%'
+
+--O setor de vendas quer fazer uma promoção para todos os clientes que são pessoas jurídicas. Para isso você deve exibir o nome dos clientes que sejam pessoa jurídica.

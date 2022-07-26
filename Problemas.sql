5c53f35d93fc4f3ccb8bf6ebf46960f34df2e901
Fabiola Spredemann <fabispredemann@gmail.com>
2022-07-26 12:35:11 -0300

    Problemas SQL - Beecrowd

--- Problemas.sql
+++ Problemas.sql
@@ -109,3 +109,11 @@ and providers.name like '%P%'
 select customers.name
 from customers, legal_person
 where customers.id = legal_person.id_customers
+
+--O setor de vendas precisa de um relatório para saber quais produtos estão sobrando em estoque.
+--Para você ajudar o setor de vendas, exiba o nome do produto e o nome da categoria, para os produtos cuja quantidade seja maior que 100 e o código da categoria seja 1,2,3,6 ou 9. Mostre essas informações em ordem crescente pelo código da categoria.
+select products.name, categories.name
+from products, categories
+where products.id_categories = categories.id and 
+products.amount > 100 and categories.id in (1,2,3,6,9)
+order by categories.id asc

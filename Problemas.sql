4d5540fcb7dd63c014579ef1f4e47fc512c4f77b
Fabiola Spredemann <fabispredemann@gmail.com>
2022-08-11 19:23:06 -0300

    Problemas SQL

--- Problemas.sql
+++ Problemas.sql
@@ -127,3 +127,7 @@ order by categories.id asc
 --Para fazer isso você deve exibir a quantidade de cidades distintas da tabela clientes.
 select count(distinct customers.city) 
 from customers
+
+--O diretor da Mangojata Advogados ordenou que lhe fosse entregue um relatório sobre seus advogados atuais.
+--O diretor quer que você mostre para ele o nome do advogado que têm mais clientes, o nome do advogado que tem menos clientes e a média de clientes entre todos os advogados.
+--OBS: Antes de apresentar a média mostre um campo chamado Average a fim de deixar o relatório mais apresentável. A média deverá ser apresentada em inteiros.
